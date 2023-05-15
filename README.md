5f42f6b44fbd753c560457cf5c60ab6cc95ad6b5
Sanzhar Zhanibekov <190103039@stu.sdu.edu.kz>
2023-05-16 01:09:29 +0600

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Education System 
+
+## [Documentation](https://silk-driver-fda.notion.site/Education-System-938a8e40ebf3427c9b312dd87f8765be)
---
+++ src/test/java/kz/acmelabs/educationsystem/EducationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.acmelabs.educationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
